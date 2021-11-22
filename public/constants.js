c89c9a3a506debb2843dffa8d6e894750b36c156
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-11-22 20:15:37 +0600

    Added new post

--- public/constants.js
+++ public/constants.js
@@ -1,3 +1,3 @@
 export const Description = "Hey, I'm Adit, a computer fanatic always trying to learn more about the amazing world of computers and tech.";
 export const Name = "Adit Chakraborty";
-export const SiteTitle = "Adit's Blog Journal"
+export const SiteTitle = "Adit's Blog Journal";
